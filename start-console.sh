d647ea43c4e562a5a65e78fd693d0ab1adb8ef83
Ugo Palatucci <upalatuc@redhat.com>
2026-01-27 11:20:15 +0100

    OCPBUGS-74423: migrate from yarn to npm

--- start-console.sh
+++ start-console.sh
@@ -21,13 +21,13 @@ for arg in $@; do
     fi
 
     git pull
-    yarn
+    npm ci
 
     if [ "$(lsof -t -i:$INITIAL_PORT)" != "" ]; then
         kill -9 $(lsof -t -i:$INITIAL_PORT)
     fi
 
-    PORT=$INITIAL_PORT yarn start --port=$INITIAL_PORT &
+    PORT=$INITIAL_PORT npm run start -- --port=$INITIAL_PORT &
 
     runningPlugins["podman-linux"]+=",${arg}=http://localhost:${INITIAL_PORT}"
     runningPlugins["podman"]+=",${arg}=http://host.containers.internal:${INITIAL_PORT}"
