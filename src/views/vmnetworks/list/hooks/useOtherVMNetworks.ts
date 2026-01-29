6be28078a894444a68e122fe88463d6735938c74
adamviktora <viktora.adam@gmail.com>
2026-01-29 13:50:22 +0100

    CNV-78079: Virtual machine networks page - Other types tab

---
+++ src/views/vmnetworks/list/hooks/useOtherVMNetworks.ts
@@ -0,0 +1,52 @@
+import {
+  ClusterUserDefinedNetworkModelGroupVersionKind,
+  NetworkAttachmentDefinitionModelGroupVersionKind,
+  UserDefinedNetworkModelGroupVersionKind,
+} from '@kubevirt-ui/kubevirt-api/console';
+import { useK8sWatchResource } from '@openshift-console/dynamic-plugin-sdk';
+import { NetworkAttachmentDefinitionKind } from '@utils/resources/nads/types';
+import { ClusterUserDefinedNetworkKind, UserDefinedNetworkKind } from '@utils/resources/udns/types';
+
+import { VALID_OTHER_VM_NETWORK_TYPES } from '../constants';
+import { OtherVMNetwork, OtherVMNetworkWithType } from '../types';
+import { getVMNetworkType, hasUDNOwner } from '../utils';
+
+const useOtherVMNetworks = (): [OtherVMNetworkWithType[], boolean, Error] => {
+  const [cudns, cudnsLoaded, cudnsError] = useK8sWatchResource<ClusterUserDefinedNetworkKind[]>({
+    groupVersionKind: ClusterUserDefinedNetworkModelGroupVersionKind,
+    isList: true,
+  });
+
+  const [udns, udnsLoaded, udnsError] = useK8sWatchResource<UserDefinedNetworkKind[]>({
+    groupVersionKind: UserDefinedNetworkModelGroupVersionKind,
+    isList: true,
+  });
+
+  const [nads, nadsLoaded, nadsError] = useK8sWatchResource<NetworkAttachmentDefinitionKind[]>({
+    groupVersionKind: NetworkAttachmentDefinitionModelGroupVersionKind,
+    isList: true,
+  });
+
+  const nadsWithoutUDN = nads?.filter((nad) => !hasUDNOwner(nad));
+
+  const otherVMNetworks: OtherVMNetwork[] = [
+    ...(cudns ?? []),
+    ...(udns ?? []),
+    ...(nadsWithoutUDN ?? []),
+  ];
+
+  const otherVMNetworksWithType: OtherVMNetworkWithType[] = otherVMNetworks
+    .map((network) => ({
+      ...network,
+      type: getVMNetworkType(network),
+    }))
+    .filter((network) => VALID_OTHER_VM_NETWORK_TYPES.has(network.type));
+
+  return [
+    otherVMNetworksWithType,
+    nadsLoaded && cudnsLoaded && udnsLoaded,
+    nadsError || cudnsError || udnsError,
+  ];
+};
+
+export default useOtherVMNetworks;
