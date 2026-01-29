9d0d70cd233e545e02d2b7e22575b44297ee0589
adamviktora <viktora.adam@gmail.com>
2026-01-29 13:50:22 +0100

    CNV-78079: Virtual machine networks page - Other types tab

--- src/views/vmnetworks/list/constants.ts
+++ src/views/vmnetworks/list/constants.ts
@@ -1,5 +1,7 @@
 import { VM_NETWORKS_OTHER_TYPES_PATH, VM_NETWORKS_PATH } from '../constants';
 
+import { VMNetworkType } from './types';
+
 export const TAB_INDEXES = {
   OTHER_VM_NETWORK_TYPES: 1,
   OVN_LOCALNET: 0,
@@ -14,3 +16,12 @@ export const PATH_BY_TAB_INDEX = {
   [TAB_INDEXES.OTHER_VM_NETWORK_TYPES]: VM_NETWORKS_OTHER_TYPES_PATH,
   [TAB_INDEXES.OVN_LOCALNET]: VM_NETWORKS_PATH,
 };
+
+export const VALID_OTHER_VM_NETWORK_TYPES = new Set([
+  VMNetworkType.LINUX_BRIDGE,
+  VMNetworkType.PRIMARY_UDN,
+  VMNetworkType.SECONDARY_LAYER2_OVERLAY,
+  VMNetworkType.SRIOV,
+]);
+
+export const NADS_LIST_PATH = '/k8s/all-namespaces/k8s.cni.cncf.io~v1~NetworkAttachmentDefinition';
---
+++ src/views/vmnetworks/list/hooks/useOtherVMNetworks.ts
@@ -0,0 +1,56 @@
+import { useMemo } from 'react';
+
+import {
+  ClusterUserDefinedNetworkModelGroupVersionKind,
+  NetworkAttachmentDefinitionModelGroupVersionKind,
+  UserDefinedNetworkModelGroupVersionKind,
+} from '@kubevirt-ui/kubevirt-api/console';
+import { useK8sWatchResource } from '@openshift-console/dynamic-plugin-sdk';
+import { NetworkAttachmentDefinitionKind } from '@utils/resources/nads/types';
+import { ClusterUserDefinedNetworkKind, UserDefinedNetworkKind } from '@utils/resources/udns/types';
+
+import { VALID_OTHER_VM_NETWORK_TYPES } from '../constants';
+import { OtherVMNetwork, OtherVMNetworkWithType } from '../types';
+import { getVMNetworkType, hasUDNOwner } from '../utils';
+
+const useOtherVMNetworks = (): [OtherVMNetworkWithType[], boolean, Error] => {
+  const [cudns, cudnsLoaded, cudnsError] = useK8sWatchResource<ClusterUserDefinedNetworkKind[]>({
+    groupVersionKind: ClusterUserDefinedNetworkModelGroupVersionKind,
+    isList: true,
+  });
+
+  const [udns, udnsLoaded, udnsError] = useK8sWatchResource<UserDefinedNetworkKind[]>({
+    groupVersionKind: UserDefinedNetworkModelGroupVersionKind,
+    isList: true,
+  });
+
+  const [nads, nadsLoaded, nadsError] = useK8sWatchResource<NetworkAttachmentDefinitionKind[]>({
+    groupVersionKind: NetworkAttachmentDefinitionModelGroupVersionKind,
+    isList: true,
+  });
+
+  const otherVMNetworksWithType: OtherVMNetworkWithType[] = useMemo(() => {
+    const nadsWithoutUDN = nads?.filter((nad) => !hasUDNOwner(nad));
+
+    const otherVMNetworks: OtherVMNetwork[] = [
+      ...(cudns ?? []),
+      ...(udns ?? []),
+      ...(nadsWithoutUDN ?? []),
+    ];
+
+    return otherVMNetworks
+      .map((network) => ({
+        ...network,
+        type: getVMNetworkType(network),
+      }))
+      .filter((network) => VALID_OTHER_VM_NETWORK_TYPES.has(network.type));
+  }, [cudns, nads, udns]);
+
+  return [
+    otherVMNetworksWithType,
+    nadsLoaded && cudnsLoaded && udnsLoaded,
+    nadsError || cudnsError || udnsError,
+  ];
+};
+
+export default useOtherVMNetworks;
