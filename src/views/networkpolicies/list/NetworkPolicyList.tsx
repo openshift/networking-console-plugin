ce214f02a2275117aa01d13e959187ce05ae3320
Parthiv Krishnan <parthiv.krishnan@gmail.com>
2026-08-10 14:17:39 -0400

    OCPBUGS-82108: marked strings for i18n in NetworkPolicies list page

--- src/views/networkpolicies/list/NetworkPolicyList.tsx
+++ src/views/networkpolicies/list/NetworkPolicyList.tsx
@@ -15,6 +15,7 @@ import ListEmptyState from '@utils/components/ListEmptyState/ListEmptyState';
 import { FLAGS } from '@utils/constants';
 import { getNetworkPolicyDocURL } from '@utils/constants/documentation';
 import { SHARED_DEFAULT_PATH_NEW_RESOURCE_FORM } from '@utils/constants/ui';
+import { useNetworkingTranslation } from '@utils/hooks/useNetworkingTranslation';
 import usePagination from '@utils/hooks/usePagination/usePagination';
 import { paginationDefaultValues } from '@utils/hooks/usePagination/utils/constants';
 import { getResourceURL } from '@utils/resources/shared';
@@ -31,6 +32,7 @@ type NetworkPolicyListProps = {
 };
 
 const NetworkPolicyList: FC<NetworkPolicyListProps> = ({ namespace }) => {
+  const { t } = useNetworkingTranslation();
   const [networkPolicies, loaded, loadError] = useK8sWatchResource<
     IoK8sApiNetworkingV1NetworkPolicy[]
   >({
@@ -96,6 +98,11 @@ const NetworkPolicyList: FC<NetworkPolicyListProps> = ({ namespace }) => {
               page={pagination?.page}
               perPage={pagination?.perPage}
               perPageOptions={paginationDefaultValues}
+              titles={{
+                itemsPerPage: t('Items per page'),
+                ofWord: t('of'),
+                perPageSuffix: t('per page'),
+              }}
             />
           )}
         </div>
